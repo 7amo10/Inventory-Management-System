13d5e7301f681ab8770dd88b37bacc8cc4dce571
Ahmed Shreiff= <ahmedshreifamer@gmail.com>
2024-10-13 08:37:03 +0300

    handle purchases

--- resources/views/purchases/index.blade.php
+++ resources/views/purchases/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.tabler')
-@section('title' , 'All Purchases')
+@section('title', 'All Purchases')
 
 @section('content')
 <div class="page-body">
@@ -21,52 +21,98 @@
                         </div>
 
                         <div class="card-actions">
-                            <a href="{{ route('purchases.create') }}" class="btn btn-icon btn-outline-success">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
+                            <a href="{{ route('purchases.create') }}"
+                                class="btn btn-primary btn btn-outline-success btn-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24"
+                                    height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M12 5l0 14" />
+                                    <path d="M5 12l14 0" />
+                                </svg>
                             </a>
                         </div>
                     </div>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Supplier</th>
-                        <th>Date</th>
-                        <th>Total</th>
-                        <th>Status</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($purchases as $purchase)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $purchase->supplier->name }}</td> <!-- Assuming supplier model has 'name' -->
-                            <td>{{ $purchase->date }}</td>
-                            <td>${{ $purchase->total_amount }}</td>
-                            <td>{{ $purchase->status }}</td>
-                            <td>
-                                <form action="{{ route('purchases.approve', $purchase->uuid) }}" method="POST"
-                                    style="display:inline;">
-                                    @csrf
-                                    <button type="submit" class="btn btn-success btn-sm">Approve</button>
-                                </form>
-                                <a href="{{ route('purchases.show', $purchase->uuid) }}"
-                                    class="btn btn-secondary btn-sm">Show</a>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Supplier</th>
+                                <th>Date</th>
+                                <th>Total</th>
+                                <th>Status</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($purchases as $purchase)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $purchase->supplier->name }}</td> <!-- Assuming supplier model has 'name' -->
+                                    <td>{{ $purchase->date }}</td>
+                                    <td>${{ $purchase->total_amount }}</td>
+                                    <td>{{ $purchase->status }}</td>
+                                    <td>
+                                        <form action="{{ route('purchases.approve', $purchase->uuid) }}" method="POST"
+                                            style="display:inline;">
+                                            @csrf
+                                            <button type="submit" class="btn btn-primary btn btn-outline-success btn-icon"><svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    class="icon icon-tabler icon-tabler-check" width="24" height="24"
+                                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                                    stroke-linecap="round" stroke-linejoin="round">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path d="M5 12l5 5l10 -10" />
+                                                </svg>
+                                            </button>
+                                        </form>
+                                        <a href="{{ route('purchases.show', $purchase->uuid) }}"
+                                            class="btn btn-icon btn-outline-info">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye"
+                                                width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                stroke="currentColor" fill="none" stroke-linecap="round"
+                                                stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                                                <path
+                                                    d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+                                            </svg>
+                                        </a>
 
-                                <a href="{{ route('purchases.edit', $purchase->uuid) }}" class="btn btn-info btn-sm">Edit</a>
-                                <form action="{{ route('purchases.delete', $purchase->uuid) }}" method="POST"
-                                    style="display:inline;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+
+                                        <a href="{{ route('purchases.edit', $purchase->uuid) }}"
+                                            class="btn btn-primary btn btn-outline-warning btn-icon"><svg
+                                                xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil"
+                                                width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                stroke="currentColor" fill="none" stroke-linecap="round"
+                                                stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"></path>
+                                                <path d="M13.5 6.5l4 4"></path>
+                                            </svg></a>
+                                        <form action="{{ route('purchases.delete', $purchase->uuid) }}" method="POST"
+                                            style="display:inline;">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-primary btn btn-outline-danger btn-icon"><svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    class="icon icon-tabler icon-tabler-trash" width="24" height="24"
+                                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                                    stroke-linecap="round" stroke-linejoin="round">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                    <path d="M4 7l16 0"></path>
+                                                    <path d="M10 11l0 6"></path>
+                                                    <path d="M14 11l0 6"></path>
+                                                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+                                                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+                                                </svg></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
         @endif
-    </div>
-</div>
-@endsection
+            </div>
+        </div>
+        @endsection
